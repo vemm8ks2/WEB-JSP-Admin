35f220f571088e04757b085efbe91357b5c34d4b
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-21 20:32:00 +0900

    docs: vite.config.js 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +13,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        dir: './dist',
-        entryFileNames: "admin-[name].min.js",
-        assetFileNames: "admin-[name].[ext]",
+        dir: "./dist",
+        entryFileNames: "[name].min.js",
+        assetFileNames: "[name].[ext]",
       },
-    }
-  }
-})
+    },
+  },
+});
